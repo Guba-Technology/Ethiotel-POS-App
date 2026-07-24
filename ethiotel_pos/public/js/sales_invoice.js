81018a40fc07e0f635d4be9200094681adb9dcf7
kelemul <kelemu3509@gmail.com>
2026-07-24 10:32:55 +0300

    sales invoice js modifies

--- ethiotel_pos/public/js/sales_invoice.js
+++ ethiotel_pos/public/js/sales_invoice.js
@@ -1,11 +1,13 @@
 frappe.ui.form.on("Sales Invoice", {
-    onload: function(frm) {
-       //check if disable_rounded_total field  exists
-        if(frm.fields_dict.disable_rounded_total){
+    refresh: function(frm) {
+        if (frm.fields_dict.disable_rounded_total) {
             console.log("disable_rounded_total exists");
-            frm.set_df_property("disable_rounded_total","checked",1);
-        frm.set_df_property("disable_rounded_total", "read_only", 1); 
+            
+            frm.set_value("disable_rounded_total", 1);
+            
+            frm.set_df_property("disable_rounded_total", "read_only", 1); 
+            
+            frm.refresh_field("disable_rounded_total");
         }
-       
     }
 });
